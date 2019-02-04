dec4701be8a0da1b1235f89c71c28370caee6231
Fran Bernabeu <Usuario@192.168.1.23>
2019-02-04 23:56:12 +0100

    versión refactorizada

--- geometria/src/Circulo_FBA.java
+++ geometria/src/Circulo_FBA.java
@@ -1,10 +1,10 @@
 
-public class Circulo extends FiguraGeometrica {
+public class Circulo_FBA extends FiguraGeometrica_FBA {
 	private double radio;
 	//Constante pi
 	static final double PI = 3.1416;
 	
-	public Circulo(double r, String tipoFigura) {
+	public Circulo_FBA(double r, String tipoFigura) {
 		super(tipoFigura);
 		radio = r;
 	}
