1aad26d0512a85df15cfda2d4b7ba9670641436a
Fran Bernabeu <Usuario@192.168.1.23>
2019-02-05 11:03:01 +0100

    nuevo método

--- geometria/src/Triangulo_FBA.java
+++ geometria/src/Triangulo_FBA.java
@@ -19,8 +19,4 @@ public class Triangulo_FBA extends FiguraGeometrica_FBA {
 		sp = this.semiPerimetro();
 		return Math.sqrt(sp * (sp-lado1) * (sp-lado2) * (sp-lado3));
 	}
-	
-	public double semiPerimetro() {
-		return this.perimetro() / 2;
-	}
 }
